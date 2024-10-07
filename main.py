ac4b05c5338498ddb1d48da070d647c88e16062e
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-10-07 16:04:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def count_letters(s): #Фукнция для подсчета букв
+    vowels = "aеёиоуыэюя" #Гласные
+    not_vowels = "бвгджзйклмнпрстфхцчшщ" #Согласные
+    vowels_amount = 0 #Инициализация количества гласных
+    not_vowels_amount = 0 #Инициализация количества согласных
+    for char in s.lower(): #Перевод к строчному виду
+        if char in vowels: #Проверка на гласную
+            vowels_amount+=1 #Добавление к счетчику гласных
+        elif char in not_vowels: #Проверка на согласную
+            not_vowels_amount +=1 #Добавление к счетчику согласных
+    return vowels_amount, not_vowels_amount #Возвращение значений
+string = input("Введие строку: ") #Ввод строки
+length= len(string) #Определение длины строки
+vowels, not_vowels = count_letters(string) #Выполнение функции
+print(f"Длина всей строки: {length}") #Вывод длины строки
+print(f"Количество гласных в строке: {vowels}") #Вывод количества гласных
+print(f"Количество согласных в строке: {not_vowels}") #Вывод количества согласных
