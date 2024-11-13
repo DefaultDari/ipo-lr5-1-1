a7205752ef23192c11cd48d338631be008a5db61
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-11-13 09:45:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-def count_letters(s): #Фукнция для подсчета букв
-    vowels = "аеёиоуыэюя" #Гласные
-    not_vowels = "бвгджзйклмнпрстфхцчшщ" #Согласные
-    vowels_amount = 0 #Инициализация количества гласных
-    not_vowels_amount = 0 #Инициализация количества согласных
-    for char in s.lower(): #Перевод к строчному виду
-        if char in vowels: #Проверка на гласную
-            vowels_amount+=1 #Добавление к счетчику гласных
-        elif char in not_vowels: #Проверка на согласную
-            not_vowels_amount +=1 #Добавление к счетчику согласных
-    return vowels_amount, not_vowels_amount #Возвращение значений
+vowels = "аеёиоуыэюя" #Гласные
+not_vowels = "бвгджзйклмнпрстфхцчшщ" #Согласные
+vowels_amount = 0 #Инициализация количества гласных
+not_vowels_amount = 0 #Инициализация количества согласных
 string = input("Введите строку: ") #Ввод строки
 length= len(string) #Определение длины строки
-vowels, not_vowels = count_letters(string) #Выполнение функции
+for char in string.lower(): #Перевод к строчному виду
+    if char in vowels: #Проверка на гласную
+        vowels_amount+=1 #Добавление к счетчику гласных
+    elif char in not_vowels: #Проверка на согласную
+        not_vowels_amount +=1 #Добавление к счетчику согласных
 print(f"Длина всей строки: {length}") #Вывод длины строки
-print(f"Количество гласных в строке: {vowels}") #Вывод количества гласных
-print(f"Количество согласных в строке: {not_vowels}") #Вывод количества согласных
+print(f"Количество гласных в строке: {vowels_amount}") #Вывод количества гласных
+print(f"Количество согласных в строке: {not_vowels_amount}") #Вывод количества согласных
